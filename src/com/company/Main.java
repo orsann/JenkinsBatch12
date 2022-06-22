02d915ad5037d2a2946bf0872d8e0792d9825273
Orsan <orsann@gmai.com>
2022-06-21 18:43:17 -0700

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         for(int i=0; i<5; i++){
-            System.out.println("Hello word");
+            System.out.println("Hello! am i a cat?");
+            System.out.println("hello! i am a dog");
         }
     }
 }
